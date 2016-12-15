e337412c3e0d4ce0069b6d24479a24cb2166a7f3
OctaM <octa.marian@gmail.com>
2016-12-15 22:01:36 +0200

    Made possible login with email

--- login/models.py
+++ login/models.py
@@ -1,10 +1,47 @@
+from time import timezone
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
 
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    mail = models.CharField(max_length=100)
-    userPhoto = models.CharField(max_length=100)
+class CustomUserManager(BaseUserManager):
+    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
 
-    def __str__(self):
+
+        if not email:
+            raise ValueError('You must enter an email')
+
+        email = self.normalize_email(email)
+        user = self.model(email = email,
+                          is_staff = is_staff, is_active = True,
+                          is_superuser = is_superuser, **extra_fields)
+        user.set_password(password)
+        user.save(using = self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, False, False, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        return  self._create_user(email, password, True, True, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=254)
+    email = models.EmailField(blank=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    objects = CustomUserManager()
+
+    def get_full_name(self):
+        
+        return self.username
+
+    def get_short_name(self):
+        "Returns the short name for the user."
         return self.username
